4248d76087d41ab3f954a63c3ba0708a662cb1cb
MajesticPaws <majesticpurr@hotmail.com>
2024-04-06 11:58:27 -0700

    Loader: initialisation sequence

--- loader/loader.go
+++ loader/loader.go
@@ -6,10 +6,26 @@ import (
 	"github.com/STCraft/DFLoader/registry"
 )
 
+// Init initialises the various libraries and utilities that may be used in common for both
+// the DFLoader and Plugins / Libraries using this loader. For example: DB. This is put in a
+// separate function so that plugins can initialise their own specific stuff after initialising
+// the loader. For example: plugins may want to initialise their tables after initialising of the
+// database.
+func Init() {
+	db.Init()
+}
+
 // Start starts the DFLoader mod and registers all the commands, libraries, and various
 // other features that this library provides.
 func Start() {
+	if db.DB == nil {
+		panic("Call loader.Init() first before calling this function")
+	}
+
+	defer func() {
+		db.DB.Close()
+	}()
+
 	registry.VanillaCommands()
-	db.Init()
 	dragonfly.New()
 }
