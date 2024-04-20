ed0c6aa61bb8afd26a2c0965ae838110a8baa48b
Crayder <crqyder@hotmail.com>
2024-04-20 23:32:52 +0530

    Fakeblock API

--- libraries/fakeblock/fakeblock.go
+++ libraries/fakeblock/fakeblock.go
@@ -1,10 +1,33 @@
 package fakeblock
 
 import (
+	"sync"
+
 	"github.com/STCraft/dragonfly/server/block/cube"
+	"github.com/STCraft/dragonfly/server/player"
 	"github.com/STCraft/dragonfly/server/world"
 )
 
+// FakeBlockRegistry is the registry of all the fake blocks spawned in the
+// server.
+type FakeBlockRegistry struct {
+	mu   *sync.Mutex
+	list []*FakeBlock
+}
+
+// fakeblocks is an instance of FakeBlockRegistry storing all the fake blocks
+// spawned in the server.
+var fakeblocks = FakeBlockRegistry{
+	mu:   &sync.Mutex{},
+	list: []*FakeBlock{},
+}
+
+// FakeBlock represents a block that is viewable by one or more specified clients.
+// Usually the use cases include virtual inventories and they usually replace air blocks.
+// Before viewing entities may go out of the scope of the fake block, you must remove it yourself
+// by calling the RemoveViewer method. It is also assumed that the original block is same for all
+// the viewing entities so that when a fakeblock is despawned, it sets it back to the original
+// block for them.
 type FakeBlock struct {
 	original world.Block
 	block    world.Block
@@ -12,24 +35,52 @@ type FakeBlock struct {
 	pos   cube.Pos
 	world *world.World
 
-	viewers []world.Viewer
+	mu      *sync.Mutex
+	viewers map[string]*player.Player
 }
 
-func New(w *world.World, pos cube.Pos, block world.Block) FakeBlock {
-	return FakeBlock{
+// Creates and returns a new FakeBlock
+func New(w *world.World, pos cube.Pos, block world.Block) *FakeBlock {
+	fb := &FakeBlock{
 		original: w.Block(pos),
 		block:    block,
 		pos:      pos,
 		world:    w,
-		viewers:  make([]world.Viewer, 0),
+		mu:       &sync.Mutex{},
+		viewers:  map[string]*player.Player{},
 	}
+
+	defer fakeblocks.mu.Unlock()
+	fakeblocks.mu.Lock()
+
+	fakeblocks.list = append(fakeblocks.list, fb)
+	return fb
 }
 
-func (fb *FakeBlock) AddViewer(v world.Viewer) {
-	fb.viewers = append(fb.viewers, v)
-	v.ViewBlockUpdate(fb.pos, fb.block, 0)
+// AddViewer adds a viewer for the fake block
+func (fb *FakeBlock) AddViewer(p *player.Player) {
+	defer fb.mu.Unlock()
+	fb.mu.Lock()
+
+	p.Session().ViewBlockUpdate(fb.pos, fb.block, 0)
+	fb.viewers[p.XUID()] = p
 }
 
-func (fb *FakeBlock) RemoveViewer(v world.Viewer) {
-	v.ViewBlockUpdate(fb.pos, fb.original, 0)
+// RemoveViewer removes the viewer for the fake block
+func (fb *FakeBlock) RemoveViewer(p *player.Player) {
+	defer fb.mu.Unlock()
+	fb.mu.Lock()
+
+	p.Session().ViewBlockUpdate(fb.pos, fb.original, 0)
+	delete(fb.viewers, p.XUID())
+}
+
+// Despawn removes the fake block for all the players in the world
+func (fb *FakeBlock) Despawn() {
+	defer fb.mu.Unlock()
+	fb.mu.Lock()
+
+	for _, p := range fb.viewers {
+		p.Session().ViewBlockUpdate(fb.pos, fb.original, 0)
+	}
 }
